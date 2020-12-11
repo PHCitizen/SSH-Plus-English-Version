726d245d871b96665963c6fca6a76bdd2c4888e9
PHCitizen <75726261+PHCitizen@users.noreply.github.com>
2020-12-10 18:46:20 -0800

    Update translate.sh

--- translate.sh
+++ translate.sh
@@ -13,32 +13,77 @@ echo "Translating............."
 sleep 2
 echo "Please Wait..............."
 echo ""
-
+echo -ne '[#                       ]\r'
 wget -q https://raw.githubusercontent.com/PHCitizen/SSH-Plus-English-Version/main/cmd/addhost -P /tmp/SSH-PLUS-ENG/
+sleep 0.3
+echo -ne '[##                      ]\r'
 wget -q https://raw.githubusercontent.com/PHCitizen/SSH-Plus-English-Version/main/cmd/alterarlimite -P /tmp/SSH-PLUS-ENG/
+sleep 0.3
+echo -ne '[###                     ]\r'
 wget -q https://raw.githubusercontent.com/PHCitizen/SSH-Plus-English-Version/main/cmd/alterarsenha -P /tmp/SSH-PLUS-ENG/
+sleep 0.3
+echo -ne '[####                    ]\r'
 wget -q https://raw.githubusercontent.com/PHCitizen/SSH-Plus-English-Version/main/cmd/attscript -P /tmp/SSH-PLUS-ENG/
+sleep 0.3
+echo -ne '[#####                   ]\r'
 wget -q https://raw.githubusercontent.com/PHCitizen/SSH-Plus-English-Version/main/cmd/badvpn -P /tmp/SSH-PLUS-ENG/
+sleep 0.3
+echo -ne '[######                  ]\r'
 wget -q https://raw.githubusercontent.com/PHCitizen/SSH-Plus-English-Version/main/cmd/banner -P /tmp/SSH-PLUS-ENG/
+sleep 0.3
+echo -ne '[#######                 ]\r'
 wget -q https://raw.githubusercontent.com/PHCitizen/SSH-Plus-English-Version/main/cmd/blockt -P /tmp/SSH-PLUS-ENG/
+sleep 0.3
+echo -ne '[########                ]\r'
 wget -q https://raw.githubusercontent.com/PHCitizen/SSH-Plus-English-Version/main/cmd/botssh -P /tmp/SSH-PLUS-ENG/
+sleep 0.3
+echo -ne '[#########               ]\r'
 wget -q https://raw.githubusercontent.com/PHCitizen/SSH-Plus-English-Version/main/cmd/conexao -P /tmp/SSH-PLUS-ENG/
+sleep 0.3
+echo -ne '[##########              ]\r'
 wget -q https://raw.githubusercontent.com/PHCitizen/SSH-Plus-English-Version/main/cmd/criarteste -P /tmp/SSH-PLUS-ENG/
+sleep 0.3
+echo -ne '[###########             ]\r'
 wget -q https://raw.githubusercontent.com/PHCitizen/SSH-Plus-English-Version/main/cmd/criarusuario -P /tmp/SSH-PLUS-ENG/
+sleep 0.3
+echo -ne '[############            ]\r'
 wget -q https://raw.githubusercontent.com/PHCitizen/SSH-Plus-English-Version/main/cmd/delhost -P /tmp/SSH-PLUS-ENG/
+sleep 0.3
+echo -ne '[#############           ]\r'
 wget -q https://raw.githubusercontent.com/PHCitizen/SSH-Plus-English-Version/main/cmd/delscript -P /tmp/SSH-PLUS-ENG/
+sleep 0.3
+echo -ne '[##############          ]\r'
 wget -q https://raw.githubusercontent.com/PHCitizen/SSH-Plus-English-Version/main/cmd/detalhes -P /tmp/SSH-PLUS-ENG/
+sleep 0.3
+echo -ne '[###############         ]\r'
 wget -q https://raw.githubusercontent.com/PHCitizen/SSH-Plus-English-Version/main/cmd/expcleaner -P /tmp/SSH-PLUS-ENG/
+sleep 0.3
+echo -ne '[################        ]\r'
 wget -q https://raw.githubusercontent.com/PHCitizen/SSH-Plus-English-Version/main/cmd/infousers -P /tmp/SSH-PLUS-ENG/
+sleep 0.3
+echo -ne '[#################       ]\r'
 wget -q https://raw.githubusercontent.com/PHCitizen/SSH-Plus-English-Version/main/cmd/menu -P /tmp/SSH-PLUS-ENG/
+sleep 0.3
+echo -ne '[##################      ]\r'
 wget -q https://raw.githubusercontent.com/PHCitizen/SSH-Plus-English-Version/main/cmd/mudardata -P /tmp/SSH-PLUS-ENG/
+sleep 0.3
+echo -ne '[###################     ]\r'
 wget -q https://raw.githubusercontent.com/PHCitizen/SSH-Plus-English-Version/main/cmd/otimizar -P /tmp/SSH-PLUS-ENG/
+sleep 0.3
+echo -ne '[####################    ]\r'
 wget -q https://raw.githubusercontent.com/PHCitizen/SSH-Plus-English-Version/main/cmd/reiniciarservicos -P /tmp/SSH-PLUS-ENG/
+sleep 0.3
+echo -ne '[#####################   ]\r'
 wget -q https://raw.githubusercontent.com/PHCitizen/SSH-Plus-English-Version/main/cmd/reiniciarsistema -P /tmp/SSH-PLUS-ENG/
+sleep 0.3
+echo -ne '[######################  ]\r'
 wget -q https://raw.githubusercontent.com/PHCitizen/SSH-Plus-English-Version/main/cmd/remover -P /tmp/SSH-PLUS-ENG/
+sleep 0.3
+echo -ne '[####################### ]\r'
 wget -q https://raw.githubusercontent.com/PHCitizen/SSH-Plus-English-Version/main/cmd/senharoot -P /tmp/SSH-PLUS-ENG/
 wget -q https://raw.githubusercontent.com/PHCitizen/SSH-Plus-English-Version/main/cmd/sshmonitor -P /tmp/SSH-PLUS-ENG/
 wget -q https://raw.githubusercontent.com/PHCitizen/SSH-Plus-English-Version/main/cmd/userbackup -P /tmp/SSH-PLUS-ENG/
+echo -ne '[########################]\r'
 
 for filename in /tmp/SSH-PLUS-ENG/*; do
     cp $filename /bin/
