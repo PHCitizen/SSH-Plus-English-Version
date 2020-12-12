45919c61a779d4d988bf1f6353ba91958a3a61f2
PHCitizen <75726261+PHCitizen@users.noreply.github.com>
2020-12-11 19:27:00 -0800

    Create ovpn-nginx.sh

---
+++ ovpn-nginx.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+clear
+echo ""
+echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo -e "\033[0;32m-        NGINX INSTALLER FOR OVPN OF SSH PLUS    -\033[0m"
+echo -e "\033[0;32m-               MADE BY PHC_Jayvee               -\033[0m"
+echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo ""
+
+sudo apt install nginx
+cp /tmp/upload-ovpn.sh /usr/bin/
+chmod +x /usr/bin/upload-ovpn.sh
+echo ""
+echo "echo done just type\033[0;32m upload-ovpn.sh\033[0m to make ovpn files downloadable thru nginx"
+echo "exiting........"
+sleep 2
