7a1973099d68c1439084539ed9baada0d15a8934
PHCitizen <75726261+PHCitizen@users.noreply.github.com>
2020-12-11 19:27:26 -0800

    Update upload-ovpn.sh

--- upload-ovpn.sh
+++ upload-ovpn.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+echo ""
+echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo -e "\033[0;32m-               MADE BY PHC_Jayvee               -\033[0m"
+echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo ""
 echo "what file you want to move in web?(eg: test.ovpn): "
 read file
 cp /root/$file /var/www/html
