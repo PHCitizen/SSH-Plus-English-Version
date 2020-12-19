866871ffac3a639709b69f98d597f15fd34aa069
PHCitizen <75726261+PHCitizen@users.noreply.github.com>
2020-12-18 19:29:01 -0800

    Update ovpn-nginx.sh

--- ovpn-nginx.sh
+++ ovpn-nginx.sh
@@ -7,7 +7,8 @@ echo -e "\033[0;32m-        NGINX INSTALLER FOR OVPN OF SSH PLUS    -\033[0m"
 echo -e "\033[0;32m-               MADE BY PHC_Jayvee               -\033[0m"
 echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 echo ""
-
+systemctl stop apache2
+fuser -k 80
 wget -q https://raw.githubusercontent.com/PHCitizen/SSH-Plus-English-Version/main/upload-ovpn.sh
 echo "wait..."
 sudo apt install nginx -y
